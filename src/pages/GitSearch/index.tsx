580b932dd76362f644c30352f24236f12450f600
Bruno Guimarães <brunoelinton@outlook.com>
2022-09-22 15:16:32 -0300

    Adjust layout configurations

--- src/pages/GitSearch/index.tsx
+++ src/pages/GitSearch/index.tsx
@@ -1,10 +1,10 @@
-import { Switch, Route } from 'react-router-dom'
-import axios from 'axios'
-import Navbar from './Navbar'
+import { Switch, Route } from 'react-router-dom';
+import axios from 'axios';
+import Navbar from './Navbar';
 
-import './styles.css'
-import { useEffect, useState } from 'react'
-import Perfil from '../../types/Perfil'
+import './styles.css';
+import { useEffect, useState } from 'react';
+import Perfil from '../../types/Perfil';
 import Repository from '../../types/Repository'
 import NameRepo_Url_Lang from '../../types/Repository'
 
@@ -40,29 +40,19 @@ const GitSearch = () => {
       .get(`https://api.github.com/users/${formData.perfil}`)
       .then((response) => {
         setPerfil(response.data)
-        console.log(response.data)
       })
       .catch((error) => {
         setPerfil(undefined)
-        console.log(error)
       })
 
     axios
       .get(`https://api.github.com/users/${formData.perfil}/repos`)
       .then((response) => {
         setRepositorio(response.data)
-        console.log(response.data)
       })
       .catch((error) => {
         setPerfil(undefined)
-        console.log(error)
       })
-
-    repositorio?.map((repo) => {
-      console.log(repo.name)
-      console.log(repo.url)
-      console.log(repo.language)
-    })
   }
 
   return (
@@ -111,7 +101,6 @@ const GitSearch = () => {
                     data-view-component="true"
                   >
                     <path
-                      fill-rule="evenodd"
                       d="M5.5 3.5a2 2 0 100 4 2 2 0 000-4zM2 5.5a3.5 3.5 0 115.898 2.549 5.507 5.507 0 013.034 4.084.75.75 0 11-1.482.235 4.001 4.001 0 00-7.9 0 .75.75 0 01-1.482-.236A5.507 5.507 0 013.102 8.05 3.49 3.49 0 012 5.5zM11 4a.75.75 0 100 1.5 1.5 1.5 0 01.666 2.844.75.75 0 00-.416.672v.352a.75.75 0 00.574.73c1.2.289 2.162 1.2 2.522 2.372a.75.75 0 101.434-.44 5.01 5.01 0 00-2.56-3.012A3 3 0 0011 4z"
                     ></path>
                   </svg>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,8 +16,6 @@ const Home = () => {
     ) as HTMLElement | null
     if (!canvasContainer) return
 
-    console.log(canvasContainer)
-
     const scene = new THREE.Scene()
     const camera = new THREE.PerspectiveCamera(
       75,
@@ -189,7 +187,6 @@ const Home = () => {
                 viewBox="0 0 16 16"
               >
                 <path
-                  fill-rule="evenodd"
                   d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"
                 />
               </svg>
